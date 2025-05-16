83e2073804f5d0934badda59e13386d622a193c5
Julia <jerlo180908@gmail.com>
2025-05-16 08:24:28 -0300

    exercício 01 - js

---
+++ prova01.js/exercicio03.js
@@ -0,0 +1,15 @@
+let opcao = 2;
+
+switch (opcao) {
+    case 1:
+        console.log("Voce escolheu Cadastrar Produto");
+        break;
+    case 2:
+        console.log("Voce escolheu Listar Produtos");
+        break;
+    case 3:
+        console.log("Voce escolheu sair");
+        break;
+    default:
+        console.log("Opcao invalida");
+} 
